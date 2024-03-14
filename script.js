db8702e2c7e5d786647835d33518b22a6949ba7f
Shohmuhammad <shohmuhammadsanaqulov@gmail>
2024-03-14 11:37:47 +0500

    4-dars

---
+++ script.js
@@ -0,0 +1,19 @@
+var a = +prompt("1dan 5gacha son kriting")
+let box=1
+for (let i = 1 ; i < 6; i++) {
+    box = box*i
+    console.log(box)
+} 
+var son = prompt("Istalgan sonni kiriting:");
+
+while (son === null && son === "" && isNaN(son)) {
+  son = +prompt("Bu son emas. Iltimos, qayta kiriting:");
+}
+
+son = Number(son);
+
+if (son % 2 === 0) {
+ son =prompt ("Bu son juft.");
+} else {
+  son = prompt("Bu son toq.");
+}
